221529f2dd7764a89fb3d73cfe1ddb13fb2bfdc4
praphul09 <praphul09@gmail.com>
2023-08-08 13:02:00 +0530

    Update ReceivedTextMessageUI.tsx

--- app/chats/[wa_id]/ReceivedTextMessageUI.tsx
+++ app/chats/[wa_id]/ReceivedTextMessageUI.tsx
@@ -1,12 +1,14 @@
 import { TextMessage } from "../../../types/Message";
-import { InteractiveMessage } from "../../../types/Message";
+import { InteractiveMessage  } from "../../../types/Message";
 import TailIn from "../TailIn";
 
-export default function ReceivedTextMessageUI(props: { textMessage: TextMessage }) {
-    const { textMessage } = props
+export default function ReceivedTextMessageUI(props: { textMessage: {} ,type:string) {
+    const { textMessage,type } = props
+    const message:string = type == "normal" ? (textMessage as TextMessage).text.body : (textMessage as InteractiveMessage).button_reply.title;
     return (
         <>
-            {textMessage.text.body}
+            
+            {message}
         </>
     )
 }
